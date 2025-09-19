f84be2557e83b737cb0614276cf7e71ac30fd19b
andre-mg <andre.m.garcia042@gmail.com>
2025-09-19 07:51:21 -0300

    delete implementation

--- src/application/Program.java
+++ src/application/Program.java
@@ -39,5 +39,9 @@ public class Program {
 		seller.setName("Martha Waine");
 		sellerDao.update(seller);
 		System.out.println("Update complete");
+		
+		System.out.println("\n === TEST 6: seller delete ===");
+		sellerDao.deleteById(11);
+		System.out.println("Delete complete");
 	}
 }
