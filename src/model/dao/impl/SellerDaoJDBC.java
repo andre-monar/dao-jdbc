c8cf14e3593aa4752be8d9ba845d404f65a948b2
andre-mg <andre.m.garcia042@gmail.com>
2025-09-19 06:54:57 -0300

    SellerDaoJDBC and DaoFactory

---
+++ src/model/dao/impl/SellerDaoJDBC.java
@@ -0,0 +1,39 @@
+package model.dao.impl;
+
+import java.util.List;
+
+import model.dao.Dao;
+
+public class SellerDaoJDBC implements Dao {
+
+	@Override
+	public void insert(Object obj) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void update(Object obj) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void deleteById(Integer id) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public Object findById(Integer Id) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public List findAll() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
