7ecfcf11f163cb11237fe8d4147a5962c946e247
andre-mg <andre.m.garcia042@gmail.com>
2025-09-19 05:24:46 -0300

    Department entity class

---
+++ src/model/entities/Department.java
@@ -0,0 +1,16 @@
+package model.entities;
+
+public class Department {
+	
+	private int id;
+	private String name;
+	
+	public Department () {
+	}
+
+	public Department(int id, String name) {
+		this.id = id;
+		this.name = name;
+	}
+	
+}
